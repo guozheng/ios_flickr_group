cb00d43f8f23b0a9ffc3a681e288b20799fc5fc5
ashwini satyanarayana <ashwini.satyanarayan@gmail.com>
2014-07-18 17:47:03 -0700

    create basic xib viewcontrollers

---
+++ ios_flickr_group/ios_flickr_group/addGroupTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  addGroupTableViewCell.m
+//  ios_flickr_group
+//
+//  Created by Ashwini Satyanarayana on 7/18/14.
+//  Copyright (c) 2014 Yahoo. All rights reserved.
+//
+
+#import "addGroupTableViewCell.h"
+
+@implementation addGroupTableViewCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ ios_flickr_group/ios_flickr_group/myGroupTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  myGroupTableViewCell.m
+//  ios_flickr_group
+//
+//  Created by Ashwini Satyanarayana on 7/18/14.
+//  Copyright (c) 2014 Yahoo. All rights reserved.
+//
+
+#import "myGroupTableViewCell.h"
+
+@implementation myGroupTableViewCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
