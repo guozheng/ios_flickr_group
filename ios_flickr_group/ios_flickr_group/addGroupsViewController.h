39cf1afd1979b76403244690a878e31a9d987f13
Li Li <liyli@yahoo-inc.com>
2014-07-26 12:02:26 -0700

    redesign addGroupsView to match myGroupView. Implement the search functionality

--- ios_flickr_group/ios_flickr_group/addGroupsViewController.h
+++ ios_flickr_group/ios_flickr_group/addGroupsViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface addGroupsViewController : UIViewController
+@interface addGroupsViewController : UIViewController<UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
 
 @end
